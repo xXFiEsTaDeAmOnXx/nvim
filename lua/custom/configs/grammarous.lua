b5032dca4ed45eae1653e94a2075d33390ff6a8d
xXFiEsTaDeAmOnXx <niklas.kleiser@web.de>
2024-09-19 14:57:50 +0200

    first commit

---
+++ lua/custom/configs/grammarous.lua
@@ -0,0 +1,2 @@
+vim.g["grammarous#jar_url"] = 'https://www.languagetool.org/download/LanguageTool-5.9.zip'
+
---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+
+-- spell checker
+vim.opt.spelllang = "de,en"
+vim.opt.spell = true
+
+
+
+
