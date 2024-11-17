1280443ba10c5a2d1b677163891b0f66cd9d764e
xXFiEsTaDeAmOnXx <niklas.kleiser@web.de>
2024-11-17 11:09:37 +0100

    Added status symbol for vimtex

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -12,6 +12,40 @@ M.base46 = {
   -- 	Comment = { italic = true },
   -- 	["@comment"] = { italic = true },
   -- },
+
+  hl_add = { -- add highlights to use for vimtex status
+    St_vtexInfo = { fg = "#61afef", bg = "NONE" },
+    St_vtexIdle = { fg = "#b294bb", bg = "NONE" },
+    St_vtexSuccess = { fg = "#98c379", bg = "NONE" },
+    St_vtexError = { fg = "#e06c75", bg = "NONE" },
+  },
+}
+
+M.ui = {
+  statusline = {
+    theme = "default",
+    separator_style = "default",
+    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "vtex", "diagnostics", "lsp", "cwd", "cursor" },
+    modules = {
+      vtex = function()
+        if vim.b.vimtex then
+          local status = vim.b.vimtex.compiler.status + 2
+
+          local symbols_and_colors = { -- use highlights specifiged ad the top and symbol
+            { "[⏻] ", "%#St_vtexInfo#" },
+            { "[⏻] ", "%#St_vtexInfo#" },
+            { "[⟳] ", "%#St_vtexIdle#" },
+            { "[✔︎] ", "%#St_vtexSuccess#" },
+            { "[✖] ", "%#St_vtexError#" },
+          }
+          local status_symbol = symbols_and_colors[status][1]
+          local color = symbols_and_colors[status][2]
+          return color .. status_symbol -- parse color string toghter to get specified color
+        end
+        return ""
+      end,
+    },
+  },
 }
 
 M.mason = {
