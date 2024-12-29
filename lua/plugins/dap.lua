acf236b5500ade46ed4db78aa6fb39fb776af6b8
xXFiEsTaDeAmOnXx <niklas.kleiser@web.de>
2024-12-29 07:49:49 +0100

    Added DAP UI Layout

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -28,7 +28,49 @@ return {
     config = function()
       local dap = require "dap"
       local dapui = require "dapui"
-      dapui.setup()
+      local userconf = {
+        layouts = {
+          {
+            elements = {
+              {
+                id = "scopes",
+                size = 0.25,
+              },
+              {
+                id = "breakpoints",
+                size = 0.25,
+              },
+              {
+                id = "stacks",
+                size = 0.25,
+              },
+              {
+                id = "watches",
+                size = 0.25,
+              },
+            },
+            position = "left",
+            size = 40,
+          },
+          {
+            elements = {
+              {
+
+                id = "console",
+                size = 0.5,
+              },
+              {
+                id = "repl",
+                size = 0.5,
+              },
+            },
+            position = "bottom",
+            size = 10,
+          },
+        },
+      }
+
+      dapui.setup(userconf)
 
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
